a6cb6b2b5d3bcfc202d3cdb1e1abec41bdd3b114
brunosz19 <hesz.bruno05@gmail.com>
2022-11-11 23:47:29 -0500

    ADD[index.js]: change and fix some mistakes [made by Martha, Sebástian, Patricia y Bruno]

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 const notes = [
-  {title: "This is the title", body:"This is the body for the note.", color: "white"},
-  {title: "This is the title", body:"This is the body for the note.", color: "green"},
-  {title: "This is the title", body:"This is the body for the note.", color: "red"},
+  {title: "first noteeeeeeeeee", body:"This is the body for the note.", color: "white"},
+  {title: "This is the title", body:"This is the body for the note.", color: "orange"},
+  {title: "This is the title", body:"This is the body for the note.", color: "pink"},
   {title: "This is the title", body:"This is the body for the note.", color: "green"},
   {title: "This is the title", body:"This is the body for the note.", color: "red"}
+];
+
+const trashNotes = [
+  {title: "This is the trash note", body:"This is the body for the note.", color: "blue"},
+  {title: "This is the trash note", body:"This is the body for the note.", color: "skyblue"}
 ];
 
 function renderNotes(notes) {
@@ -21,33 +26,58 @@ function renderNotes(notes) {
     <p class="line-height-1.25">${notes[i].body}</p>
   
     <div class="flex-row align-item__end">
-      <figure id="choseColor${i}">
+      <a href="#" id="choseColor${i}">
         <img  class="svg-custom button-circular" src="./css/imagen/icon-paleteishon.svg" alt="">
-      </figure>
-      <figure id="deleteIcon${i}">
+      </a>
+      <a href="#" id="deleteIcon${i}">
         <img class="svg-custom button-circular" src="./css/imagen/icon-trash.svg" alt="">
-      </figure>
-      <figure>
-        <img class="svg-custom button-circular" src="./css/imagen/icon-restored.svg" alt="">
-      </figure>
+      </a>
     </div>
     `
     
-    li.innerHTML = liTemplate;
+    const liTemplateTrash = `
+    <h1 class="text-sm bold">${notes[i].title}</h1>
+    <p class="line-height-1.25">${notes[i].body}</p>
+  
+    <div class="flex-row align-item__end">
+
+      <a href="#" id="deleteIcon${i}">
+        <img class="svg-custom button-circular" src="./css/imagen/icon-trash.svg" alt="">
+      </a>
+      <a href="#" id="restoreIcon${i}">
+        <img  class="svg-custom button-circular" src="./css/imagen/icon-restored.svg" alt="">
+      </a>
+    </div>
+    `
+    
+    notes == trashNotes ? li.innerHTML = liTemplateTrash : li.innerHTML = liTemplate;
+    
     ul.append(li);
     
-    document.querySelector(`#choseColor${i}`).addEventListener("click", () => {
-      console.log("aqui estoy");
+    document.querySelector(`#deleteIcon${i}`).addEventListener("click", () => {
+      notes == trashNotes ? deleteNote(i) : toTrashNote(i);
     });
     
-    document.querySelector(`#deleteIcon${i}`).addEventListener("click", () => {
-      deleteNote(i);
-    });
+     if (notes == trashNotes) {
+      document.querySelector(`#restoreIcon${i}`).addEventListener("click", () => {
+        restoreNote(i);
+      });
+    } else {
+      document.querySelector(`#choseColor${i}`).addEventListener("click", () => {
+        console.log("diste click");
+      });
+    }
   }
+
+  const choseColor =  document.querySelector(`#choseColor`)
+  choseColor.addEventListener("click", () => {
+    console.log("si esta dando click.");
+    // choseColor.className("choseColor__close");
+  });
+  
 }
 
 const form = document.querySelector("form");
-
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
@@ -55,7 +85,7 @@ event.preventDefault()
 const data = event.target.elements;
 const newNote = {
   title: data.title.value,
-  body:   data.note.value,
+  body:  data.note.value,
   color: data.color.value,
 };
 notes.push(newNote);
@@ -63,9 +93,33 @@ event.target.reset();
 renderNotes(notes);
 }
 
-renderNotes(notes);
-
-function deleteNote(i) {
+function toTrashNote(i) {
+  let noteToTrash = notes[i];
+  trashNotes.push(noteToTrash);
   notes.splice(i, 1);
   renderNotes(notes);
 }
+
+function deleteNote(i) {
+  trashNotes.splice(i, 1);
+  renderNotes(trashNotes);
+}
+
+function restoreNote(i) {
+  let noteToIndex = trashNotes[i];
+  notes.push(noteToIndex);
+  trashNotes.splice(i, 1);
+  renderNotes(trashNotes);
+}
+
+const trash = document.querySelector("#trash");
+trash.addEventListener("click", () => {
+  renderNotes(trashNotes);
+});
+
+const notesOption = document.querySelector("#notesOption");
+notesOption.addEventListener("click", () => {
+  renderNotes(notes);
+});
+
+renderNotes(notes);
